6612e2f1f0e186262f1b5122b729e21a379d82fa
OmarSD24 <o.chamchoum470@gmail.com>
2025-09-25 11:22:56 +0200

    Add files via upload
    Login files voor student en docent

---
+++ login_page.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Kanban Planner Dashboard</title>
+</head>
+<body>
+  <div class="card">
+    <h1>Welcome</h1>
+    <p>Log in als:</p>
+    <a href="student.php" class="btn btn-student">Student</a>
+    <a href="docent.php" class="btn btn-docent">Docent</a>
+  </div>
+</body>
+</html>
